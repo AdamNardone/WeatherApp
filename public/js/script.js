9e3fa098315cd03e3cc3534eaa8efa4e8ac70f6d
nardoneadam <nardoneadam@gmail.com>
2019-11-10 20:24:31 -0500

    Added hourly weather data up to 24 hours

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,29 @@
+const hourlySlide = document.querySelector(".hourly-slide");
+
+const prevBtn = document.getElementById("prev-btn");
+const nextBtn = document.getElementById("next-btn");
+
+let counter = 0;
+const size = document.querySelector(".hourly-wrapper").clientWidth;
+
+prevBtn.addEventListener("click", () => {
+  if (counter <= 0) return;
+
+  hourlySlide.style.transition = "transform 0.4s ease-in-out";
+  counter--;
+  hourlySlide.style.transform = "translateX(" + -size * counter + "px)";
+  console.log(size);
+});
+
+nextBtn.addEventListener("click", () => {
+  if (counter >= 3) return;
+
+  hourlySlide.style.transition = "transform 0.4s ease-in-out";
+  counter++;
+  hourlySlide.style.transform = "translateX(" + -size * counter + "px)";
+  console.log(size);
+});
+
 document.getElementById("todays-forecast").style.display = "none";
 document.getElementById("daily-forecast").style.display = "none";
 
@@ -61,6 +87,7 @@ function callbackFn(result) {
     .then(data => {
       setWeatherData(data, place);
       setDayHeaders(today);
+      setHourHeaders(today);
     });
 }
 
@@ -90,6 +117,7 @@ function setWeatherData(data, place) {
   )}%`;
   CurrentDayWindElement.textContent = data.currently.windSpeed;
 
+  //daily weather dat
   for (var i = 0; i < 7; i++) {
     icon.set(`icon${i + 1}`, data.daily.data[i + 1].icon);
     icon.play();
@@ -100,9 +128,17 @@ function setWeatherData(data, place) {
     )}%`;
     windElement[i].textContent = data.daily.data[i + 1].windSpeed;
   }
+
+  //hourly weather data
+  for (var i = 0; i < 24; i++) {
+    icon.set(`icon-hour-${i}`, data.hourly.data[i].icon);
+    icon.play();
+  }
 }
 
 const day = document.querySelectorAll("[week-day]");
+const hour = document.querySelectorAll("[hour-time]");
+const hourDay = document.querySelectorAll("[hour-day]");
 
 const days = [
   "Sunday",
@@ -114,12 +150,48 @@ const days = [
   "Saturday"
 ];
 
+const daysShort = ["Sun", "Mon", "Tues", "Wed", "Thur", "Fri", "Sat"];
+
+const hours = [
+  "12:00 AM",
+  "1:00 AM",
+  "2:00 AM",
+  "3:00 AM",
+  "4:00 AM",
+  "5:00 AM",
+  "6:00 AM",
+  "7:00 AM",
+  "8:00 AM",
+  "9:00 AM",
+  "10:00 AM",
+  "11:00 AM",
+  "12:00 PM",
+  "1:00 PM",
+  "2:00 PM",
+  "3:00 PM",
+  "4:00 PM",
+  "5:00 PM",
+  "6:00 PM",
+  "7:00 PM",
+  "8:00 PM",
+  "9:00 PM",
+  "10:00 PM",
+  "11:00 PM"
+];
+
 const today = new Date();
 
 function setDayHeaders(date) {
   for (var i = 0; i < 7; i++) {
     date.setDate(today.getDate() + 1);
-    console.log(date.getDay());
     day[i].textContent = days[date.getDay()];
+  }
+}
+
+function setHourHeaders(date) {
+  for (var i = 0; i < 24; i++) {
+    hour[i].textContent = hours[date.getHours()];
+    hourDay[i].textContent = daysShort[date.getDay()];
+    date.setHours(today.getHours() + 1);
   }
 }
